c6700e7456002c49f38ea17502b2200373067868
yana.s <yana.strilets.pz.2021@lpnu.ua>
2024-04-03 16:27:15 +0300

    init project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import {MainHeader} from './mainHeader';
+import houseBlock from "./houseBlock";
+import mainFooter from "./mainFooter";
+import {HouseBlock} from "./houseBlock";
+import psyacha_dushova from "./assets/images/karpaty_lviv.jpg";
+import {Login} from "./Login";
+import {
+    Routes,
+    Route,
+    Navigate, BrowserRouter,
+} from "react-router-dom";
+import {Home} from "./Home";
+/*import Login from "./Login";*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+    return (
+        <div className='main-container'>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home/>}></Route>
+                    <Route path='/login' element={<Login/>}></Route>
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
